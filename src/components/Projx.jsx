3c8afb0c1f592318e4ad370f1919abfac25b4b1b
Csalas <39786104+c-salas@users.noreply.github.com>
2023-11-04 06:17:54 -0300

    .

--- src/components/Projx.jsx
+++ src/components/Projx.jsx
@@ -8,14 +8,9 @@ export const Projx= () => {
 				<div id="main" className="wrapper style1">
                     
 					<div className="container">
-                        <section id="one" className="projJumbo style2 right">
-                            <span className="image fit main"><img src="..\assets\images\pic06.jpg" alt="" /></span>
-                         
-
-							
                             <div className="content">
-                                <header>
-                                    <h3>Samodata Landing Page</h3>
+                                <header >
+                                    <h2>Samodata Landing Page</h2>
                                         <p> Static landing page using html5 and css3 for IT client.</p>
                                 </header>
                                 <ul className="actions">
@@ -23,16 +18,16 @@ export const Projx= () => {
                                     <li><a href="https://github.com/c-salas/Samodata" class="button icon brands alt fa-github"> GitHub</a></li>
                                 </ul>
                             </div>
-
+                        <section id="one" className="projJumbo style2 right">
+                            <span className="image fit main"><img src="..\assets\images\pic06.jpg" alt="" /></span>
                         </section>
 
                         <hr/>
 
                         <section id="two" className="projJumbo style3 left">
-                            <span className="image fit main bottom"><img src="../assets/images/pic07.jpg" alt="" /></span>
-                            <div className="content">
+                        <div className="content">
                                 <header>
-                                    <h3>Coffee Cats E-Commerce</h3>
+                                    <h2>Coffee Cats E-Commerce</h2>
                                     <p>E-Commerce page using Html5/Css3, Nodejs, ExpresJs, ReactJS and MongoDB technologies.</p>
                                 </header>
                                 <ul className="actions">
@@ -40,6 +35,8 @@ export const Projx= () => {
                                     <li><a href="https://github.com/c-salas/CarritoCompras-UDD-C-salas" class="button icon brands alt fa-github">GitHub</a></li>
                                 </ul>
                             </div>
+                            <span className="image fit main bottom"><img src="../assets/images/pic07.jpg" alt="" /></span>
+                            <hr />
 
 			            </section>
 
